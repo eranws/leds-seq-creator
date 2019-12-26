313d2c4cfb4ac150b7f68adbb2b33aca0d028046
bigoren <bigoreo@gmail.com>
2019-12-26 20:53:24 +0200

    saving blind work done at home to finish nikki, need to see changes on installation

--- songs/nikki.py
+++ songs/nikki.py
@@ -14,7 +14,7 @@ from led_objects.cabbages import cabbage1, cabbage6, brain7, cabbage5, cabbages,
 from led_objects.flood import floods, cup_cakes, cup_cake3, rug4, cup_cake4, rug6, rugs
 from led_objects.led_object import all
 from led_objects.flowers import flower6, flowers, paper5, strings, flower1, bottles, papers, paper2, bottle4, bottle5, \
-    gloves8
+    gloves8, gloves
 from led_objects.objects_selector import elements
 from led_objects.sheep import sheep
 from led_objects.stands import sticks8, single_sticks, sticks7, sticks3, lifas5, lifas1, lifas4, lifas, single_lifas, \
@@ -94,14 +94,16 @@ elements(sticks8, sticks7)
 color.alternate(indigo, red)
 # additional bling in sequence
 beats(63 + 2 / 3, 70)
-elements(gloves8)
+elements(gloves8, meduza)
 color.alternate(indigo, red)
 beats(65, 70)
 elements(lifas5, lifas1)
 color.alternate(indigo, red)
 
 # stop triolas for one beat and static noise
-# beats(70, 71)
+beats(70, 71)
+elements(all)
+color.uniform(black)
 
 # stretched out background sound
 beats(88, 88 + 2/3)
@@ -174,17 +176,17 @@ elements(sticks7, sticks8)
 color.gradient(0.995, 0.61)
 
 beats(96 + 6/10, 100)
-elements(gloves8)
+elements(gloves8, meduza)
 color.gradient(0.995, 0.61)
 
 beats(98, 100)
-elements(lifas1)
+elements(lifas1,sheep)
 color.gradient(0.995, 0.61)
 
 # episode 3, triolas, drum and some specific maps
 n_episodes(3, 4)
 elements(bottles, flowers, papers, donuts, stars, twists)
-color.uniform(indigo)
+color.uniform(turquoise_string)
 cycle(8)
 cycle_beats(0, 2)
 elements(bottles, paper5)
@@ -220,7 +222,7 @@ beats(112.66, 113)
 elements(lifas1.stand(4), lifas4.stand(4), lifas5.stand(4))
 color.uniform(coral)
 
-# episode 3 small sounds middle
+# episode 3 small sounds end
 beats(120, 129)
 elements(sticks3.stand(1), sticks7.stand(1), sticks8.stand(1))
 color.uniform(white)
@@ -240,7 +242,7 @@ color.uniform(green)
 # episode 4 triolas and drum on 2 beats
 n_episodes(4, 5)
 elements(bottles, flowers, papers, donuts, stars, twists)
-color.uniform(indigo)
+color.uniform(turquoise_string)
 cycle(8)
 cycle_beats(0, 2)
 elements(bottles, paper5)
@@ -263,7 +265,7 @@ color.uniform(magenta)
 effect.saw_tooth()
 
 beats(138, 139)
-elements(gloves8)
+elements(gloves8, meduza)
 color.uniform(green)
 beats(144, 145)
 elements(lifas4)
@@ -284,13 +286,13 @@ beats(160, 163)
 elements(sticks7, sticks8)
 color.uniform(green)
 beats(161, 163)
-elements(gloves8)
+elements(gloves8, sheep)
 color.uniform(green)
 
 # episode 5 triolas and drum fades, background increases
 n_episodes(5, 6)
 elements(bottles, flowers, papers, donuts, stars, twists)
-color.uniform(indigo)
+color.uniform(turquoise_string)
 cycle(8)
 cycle_beats(0, 2)
 elements(bottles, paper5)
@@ -357,10 +359,10 @@ cycle_beats(6, 8)
 elements(twists)
 effect.snake()
 
-n_episodes(6, 6.25)
-elements(bottles, flowers, papers, donuts, stars, twists)
-effect.brightness(0.5)
-effect.fade_in()
+# n_episodes(6, 6.25)
+# elements(bottles, flowers, papers, donuts, stars, twists)
+# effect.brightness(0.5)
+# effect.fade_in()
 
 # episode 7 add violin1
 n_episodes(7, 8.875)
@@ -404,8 +406,10 @@ effect.fade_in()
 
 # big rise for drop
 n_episodes(8.875, 9)
-elements(all)
+elements(all, meduza, sheep)
 color.uniform(indigo)
+# add the half brightness if we dont want the three rises
+# effect.brightness(0.5)
 fade_in_parameteric(1.0, 2.0)
 
 # episode 9 drama
@@ -426,7 +430,7 @@ effect.hue_shift(-0.2)
 effect.brightness(0.5)
 
 # episode 10 triolas come back still with drama
-n_episodes(10, 10.75)
+n_episodes(10, 10 + 15/16)
 elements(bottles, flowers, papers, lifas, sticks, gloves8)
 color.uniform(pink_string)
 cycle(8)
@@ -466,7 +470,7 @@ color.uniform(turquoise_strip)
 effect.blink()
 
 # end of episode blinks
-elements(all)
+elements(twists,stars, donuts, floods, meduza, sheep)
 beats(345, 351)
 color.gradient(0.61, 0.995)
 effect.blink_repeat(32)
@@ -476,7 +480,9 @@ effect.hue_shift(-0.2)
 effect.brightness(0.5)
 
 beats(351, 353)
+elements(all, meduza, sheep)
 color.uniform(aquamarine)
+effect.brightness(0.7)
 effect.fade_in()
 
 # episode 11
@@ -572,7 +578,7 @@ effect.hue_shift(-0.2)
 # effect.brightness(0.5)
 
 beats_in_episode(12, 30, 31)
-elements(gloves8)
+elements(gloves8, meduza)
 color.gradient(0.4, 0.8)
 effect.saw_tooth()
 
@@ -620,7 +626,7 @@ effect.brightness(0.5)
 n_episodes(13, 16)
 for e in all:
     e.random
-elements(bottles, flowers, papers, donuts, stars)
+elements(bottles, flowers, papers, donuts)
 color.gradient(0.6, 1.0)
 effect.brightness(0.5)
 cycle(8)
@@ -632,25 +638,68 @@ cycle_beats(2, 4)
 elements(flowers)
 effect.fill_out_in()
 cycle_beats(4, 6)
-elements(donuts, stars)
+elements(donuts)
 effect.fill_out_in()
 cycle_beats(6, 8)
 elements(papers)
 effect.fill_out_in()
 
+# episode 14
+# enter low violin
+n_episodes(14, 15)
+elements(stars, brains, gloves, meduza, sheep)
+color.uniform(dark_blue)
+cycle(2)
+effect.brightness(0.5)
+effect.saw_tooth()
+effect.hue_shift_cycle_diff(0.0, 0.01)
+
 # episode 15
-# enter violin
+# enter high violin
+n_episodes(15, 16 + 15/16)
+elements(single_stands)
+color.gradient(0.7, 0.9)
+cycle(2)
+effect.brightness(0.5)
+effect.saw_tooth()
+effect.fill_out_in()
+effect.hue_shift_cycle_diff(0.0, 0.01)
 
 # episode 16
 # enter second triola
+n_episodes(16, 16 + 15/16)
+elements(no_stands)
+color.gradient(0.8, 1.0)
+effect.brightness(0.5)
+cycle(8)
+effect.hue_shift_cycle_diff(0.0, 0.04)
+cycle_beats(0, 2)
+elements(bottles, brains, cabbages)
+effect.fill_out_in()
+cycle_beats(2, 4)
+elements(flowers, gloves, rugs)
+effect.fill_out_in()
+cycle_beats(4, 6)
+elements(donuts, stars)
+effect.fill_out_in()
+cycle_beats(6, 8)
+elements(papers, cup_cakes)
+effect.fill_out_in()
 
 # background increasing to explosion at end of episode
+n_episodes(16.5, 16 + 15/16)
+elements(all)
+fade_in_parameteric(1.0, 1.5)
+
+beats_in_episode(16, 31, 33)
+color.uniform(red)
+effect.fade_in()
 
 # episodes 17, 18 drama
 n_episodes(17, 17 + 15/16)
 cycle(4)
 cycle_beats(0, 2)
-elements(no_stands)
+elements(no_stands, meduza, sheep)
 color.uniform(yellow_strip)
 effect.blink_repeat(32)
 cycle_beats(2, 4)
@@ -664,9 +713,8 @@ FillAnimation(ConstFloatFunc(0.0), StepsFloatFunc(6, 1/6, 1/6)).apply()
 cycle_beats(6, 8)
 FillAnimation(ConstFloatFunc(0.0), StepsFloatFunc(6, -1/6, 1)).apply()
 
-
 cycle(4)
-elements(all)
+elements(all, meduza, sheep)
 effect.hue_shift_cycle_diff(0.0, 0.2)
 
 n_episodes(17 + 15/16, 18)
@@ -677,7 +725,7 @@ effect.blink_repeat(3)
 n_episodes(18, 18 + 15/16)
 cycle(4)
 cycle_beats(0, 2)
-elements(no_stands)
+elements(no_stands, meduza, sheep)
 color.uniform(yellow_strip)
 effect.blink_repeat(32)
 cycle_beats(2, 4)
@@ -692,7 +740,7 @@ cycle_beats(6, 8)
 FillAnimation(ConstFloatFunc(0.0), StepsFloatFunc(6, -1/6, 1)).apply()
 
 cycle(4)
-elements(all)
+elements(all, meduza, sheep)
 effect.hue_shift_cycle_diff(0.0, 0.2)
 
 n_episodes(18 + 15/16, 19)
@@ -700,9 +748,9 @@ elements(single_stands)
 color.gradient(0.6, 0.8)
 effect.blink_repeat(3)
 
-# episode 19 start fade out
+# episode 19 start fade out until 20.5
 n_episodes(19, 20.5)
-elements(no_stands)
+elements(no_stands, meduza, sheep)
 color.uniform(red)
 BrightnessAnimation(StepsFloatFunc(12, -1/12, 1)).apply()
 cycle(4)
@@ -719,15 +767,53 @@ FillAnimation(ConstFloatFunc(0.0), StepsFloatFunc(3, -1/3, 1)).apply()
 cycle(4)
 effect.hue_shift_cycle_diff(0.0, 0.02)
 
-# episode 20 fade, only blings, start slowing half way
+# episode 20.5 fade, only slowing down blings
+beats(658, 662.5)
+elements(group1, group2)
 color.gradient(0.2, 0.4)
-end_index = 657
-for i in range(0, 7):
-    start_index = end_index
-    end_index = start_index + 3 + i*0.15
-    beats(start_index, end_index)
+
+beats(658.66, 663.2)
+elements(group3, group8)
+color.gradient(0.2, 0.4)
+
+beats(660.1, 664.9)
+elements(group4, group7)
+color.gradient(0.2, 0.4)
+
+beats(660.9, 665.8)
+elements(group5, group6)
+color.gradient(0.2, 0.4)
+
+beats(667.5, 673.8)
+elements(group1, group2)
+color.gradient(0.2, 0.4)
+
+beats(668.5, 675)
+elements(group3, group8)
+color.gradient(0.2, 0.4)
+
+beats(670.5, 677.5)
+elements(group4, group7)
+color.gradient(0.2, 0.4)
+
+beats(671.5, 678.8)
+elements(group5, group6)
+color.gradient(0.2, 0.4)
 
 # episode 21 slowing blings continue and fin!
+# give an extra touch to a different bling towards the end
+beats(673.8, 675)
+elements(meduza, sheep)
+color.gradient(0.2, 0.4)
+
+# final bling is meduza and kivsi only
+beats(681.5, 686)
+elements(meduza)
+color.uniform(indigo)
+
+elements(sheep)
+color.uniform(white)
+
 
 send_to_mqtt("nikki")
 start_song("nikki", 0)
