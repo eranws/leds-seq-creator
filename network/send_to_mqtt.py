b91afb55b0b19cf6909051dd2e381d628fa9ad77
eran ws <eranws@gmail.com>
2022-09-10 17:28:22 +0300

    remove animations_json_compact (unused)

--- network/send_to_mqtt.py
+++ network/send_to_mqtt.py
@@ -37,27 +37,10 @@ def send_to_mqtt(filename):
     client.on_publish = on_publish_callback
 
     for led_object, thing_name in obj_to_thing.items():
-
-        float_functions_store.reset()
-        boolean_functions_store.reset()
-        discrete_float_functions_store.reset()
-
         animations_json = [an.to_json_obj(False) for an in led_object.animations]
-
-        animations_json_compact = [an.to_json_obj(True) for an in led_object.animations]
-        compact_json = {"float_funcs": float_functions_store.saved_func_by_index,
-                        "boolean_funcs": boolean_functions_store.saved_func_by_index,
-                        "discrete_float_funcs": discrete_float_functions_store.saved_func_by_index,
-                        "animations": animations_json_compact}
-
         json_str = json.dumps(animations_json, separators=(',', ':')) + '\0'
-        json_str_compact = json.dumps(compact_json, separators=(',', ':')) + '\0'
-
-        print(f"sending json to thing {thing_name} size: {len(json_str)}, compact: {len(json_str_compact)}")
         print(json_str)
         msg_info = client.publish("animations/{}/{}".format(thing_name, filename), json_str, qos=1)
-        sent_not_acked.add(msg_info.mid)
-        msg_info = client.publish("animations_c/{}/{}".format(thing_name, filename), json_str_compact, qos=1)
         sent_not_acked.add(msg_info.mid)
 
     client.loop_forever(10)
