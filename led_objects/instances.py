2d2bf3d9504ce139caa2a222406eae24ee2d960a
eran ws <eranws@gmail.com>
2022-09-10 17:22:43 +0300

    map all instances to flower1 and cabbage1

--- led_objects/instances.py
+++ led_objects/instances.py
@@ -1,35 +1,11 @@
-from led_objects.led_object import LedObject
+from led_objects.led_object import LedObject, SegmentProxy
 from led_objects.flowers import Flower
 from led_objects.stands import Stands
 from led_objects.stars import Star
 
 # cabbages:
-cabbage1 = LedObject(10) # 50
-cabbage6 = LedObject(10) # 50
-cabbage5 = LedObject(10) # 50
-
-brain7 = LedObject(29) # 299
-cup_cake4 = LedObject(30) # 300
-
-donut1 = LedObject(29) #296
-donut3 = LedObject(29) #296
-
-# flood
-cup_cake3 = LedObject(10)
-rug6 = LedObject(30)
-rug4 = LedObject(24)
-
-flower1 = Flower(30)
-flower6 = Flower(15)
-
-bottle4 = Flower(5)
-bottle5 = Flower(5)
-
-paper2 = Flower(5, 2)
-paper5 = Flower(5, 3)
-
-gloves8 = Flower(5)
-
+flower1 = LedObject(128)
+cabbage1 = LedObject(128)
 
 sticks8 = Stands([12, 31, 46, 56, 72])
 sticks7 = Stands([16, 29, 51, 68, 87, 103])
@@ -40,5 +16,23 @@ lifas1 = Stands([45, 84, 103, 131, 164])
 lifas4 = Stands([55, 122, 178, 228])
 
 #stars:
-star7 = Star(50, 3)
-star8 = Star(25, 2)
+star7 = Star(3,1)
+star8 = Star(4,2)
+
+cabbage6 = cabbage1
+cabbage5 = cabbage1
+brain7 = cabbage1
+cup_cake4 = cabbage1
+donut1 = cabbage1
+donut3 = cabbage1
+cup_cake3 = cabbage1
+rug6 = cabbage1
+rug4 = cabbage1
+
+flower6 = flower1
+bottle4 = flower1
+bottle5 = flower1
+paper2 = flower1
+paper5 = flower1
+gloves8 = flower1
+
