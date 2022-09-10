c6294918c663fae49138ac90b5f512d561f7a188
eran ws <eranws@gmail.com>
2022-09-10 23:46:45 +0300

    objects config sender, report mqtt error

--- network/objects_config_sender.py
+++ network/objects_config_sender.py
@@ -8,23 +8,32 @@ from thing_to_obj_map import obj_to_thing
 mqtt_host_name = "10.0.0.200"
 mqtt_client_id = "objects_map_sender"
 
+client = mqtt.Client(mqtt_client_id)
+
+try:
+    client.connect(mqtt_host_name)
+except OSError as e:
+    print("Error: Mqtt client is Null:")
+    print(e)
+
+
 def ledObjectToJson(led_object):
-    return {
+    led_object_json={
         "total_pixels": led_object.total_pixels,
         "objects": led_object.mapping
     }
-
-
-client = mqtt.Client(mqtt_client_id)
-client.connect(mqtt_host_name)
+    return json.dumps(led_object_json, separators=(',', ':'))
+    
 
 def send_to_single_thing(thing_name, led_object):
-    json_str = json.dumps(ledObjectToJson(led_object), separators=(',', ':'))
+    json_str = ledObjectToJson(led_object)
     print(f"json size: {len(json_str)}")
-    print(json_str)
-
-    topic = "objects-config/" + thing_name
-    client.publish(topic, json_str)
+    print(json_str)   
+    
+    if(client):     
+        client.publish(topic="objects-config/" + thing_name, payload=json_str)
+    else:
+        print("Mqtt client is Null")
 
 def send_to_all_things():
     for led_object,thing_name in obj_to_thing.items():
